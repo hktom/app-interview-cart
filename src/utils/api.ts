0bce34128a2c3b606cae1bda065d7b6b519c1d1b
tom hikari <tomprescott1565@gmail.com>
2023-04-12 09:43:31 +0100

    feature/new_project

---
+++ src/utils/api.ts
@@ -0,0 +1,108 @@
+export interface IAPI {
+  id: string;
+  name: string;
+  price: string;
+  image: string;
+  description: string;
+}
+
+export const api: IAPI[] = [
+  {
+    id: "1",
+    name: "Big Mac",
+    price: "$3.99",
+    image:
+      "https://www.mcdonalds.com/content/dam/usa/nfl/assets/nav/arches-logo_50.png",
+    description:
+      "A mouthwatering burger with a special sauce, topped with lettuce, cheese, pickles and onions.",
+  },
+  {
+    id: "2",
+    name: "Whopper",
+    price: "$4.99",
+    image:
+      "https://www.bk.com/sites/default/files/2021-04/new-whopper-web-hero_0.png",
+    description:
+      "A flame–grilled patty, topped with tomatoes, onions, lettuce, pickles, ketchup, and mayonnaise, served on a sesame seed bun.",
+  },
+  {
+    id: "3",
+    name: "Original Chicken Sandwich",
+    price: "$5.99",
+    image:
+      "https://www.bk.com/sites/default/files/2021-04/original-chicken-sandwich-web-hero.png",
+    description:
+      "Juicy, tender and crispy chicken fillet, topped with lettuce and creamy mayonnaise sauce, served on a toasted sesame-seed bun.",
+  },
+  {
+    id: "4",
+    name: "Classic Cheeseburger",
+    price: "$2.99",
+    image:
+      "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/nav/arches-logo_50.png",
+    description:
+      "Melted American cheese, pickles, onions, mustard, and ketchup, all on a soft, fluffy bun with a 100% pure beef patty.",
+  },
+  {
+    id: "5",
+    name: "Famous Bowl",
+    price: "$6.99",
+    image:
+      "https://www.kfc.com/hubfs/2021%20Menus%20Menu/Menu-Identify/Famous-Bowl.png",
+    description:
+      "A cheesy blend of golden mashed potatoes, gravy, crispy popcorn chicken, corn, and shredded cheese.",
+  },
+  {
+    id: "6",
+    name: "Double Whopper",
+    price: "$7.99",
+    image:
+      "https://www.bk.com/sites/default/files/2021-04/double-whopper-web-hero.png",
+    description:
+      "Two flame-grilled beef patties piled high with tomatoes, onions, lettuce, pickles, ketchup, and mayonnaise.",
+  },
+  {
+    id: "7",
+    name: "Chicken McNuggets",
+    price: "$4.99",
+    image:
+      "https://www.mcdonalds.com/content/dam/usa/nfl/assets/nav/arches-logo_50.png",
+    description:
+      "Crispy and tender pieces of all-white meat chicken, served in bite-sized pieces with your choice of dipping sauce.",
+  },
+  {
+    id: "8",
+    name: "Impossible Whopper",
+    price: "$5.99",
+    image:
+      "https://www.bk.com/sites/default/files/2021-04/impossible-whopper-web-hero.png",
+    description:
+      "A plant-based patty made with Impossible Foods, topped with juicy tomatoes, crisp lettuce, creamy Mayonnaise, ketchup, and sliced pickles all on a toasted sesame seed bun.",
+  },
+  {
+    id: "9",
+    name: "BBQ Bacon Whopper",
+    price: "$6.99",
+    image:
+      "https://www.bk.com/sites/default/files/2021-04/bbq-bacon-whopper-web-hero.png",
+    description:
+      "A juicy beef patty topped with crispy bacon, cheese, freshly-cut onions, ripe tomatoes, and smoky BBQ sauce on a sesame seed bun.",
+  },
+  {
+    id: "10",
+    name: "10-Piece Chicken McNuggets",
+    price: "$7.99",
+    image:
+      "https://www.mcdonalds.com/content/dam/usa/nfl/assets/nav/arches-logo_50.png",
+    description:
+      "Ten crispy and tender pieces of all-white meat chicken, served in bite-sized pieces with your choice of dipping sauce.",
+  },
+  {
+    id: "11",
+    name: "Big N' Tasty",
+    price: "$4.99",
+    image:
+      "https://www.mcdonalds.com/is/image/content/dam/usa/nfl/assets/nav/arches-logo_50.png",
+    description: "A premium burger made with 100",
+  },
+];
